70d9256a43ea0480f05720b827dc580c8a8b9f0f
Douglas Bacelar <bacelardouglas@outlook.com>
2023-02-04 11:54:13 -0300

    Requisito 6 antes de refatoração do código

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+
 import Header from '../components/Header';
+import Loading from './Loading';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Cards from '../components/Cards';
 
 class Search extends Component {
   state = {
     artist: '',
+    albuns: [],
+    isLoading: false,
+    searchValue: '',
   };
 
   handleChange = ({ target }) => {
@@ -15,14 +22,35 @@ class Search extends Component {
     });
   };
 
-  render() {
+  handleClickErase = async () => {
     const { artist } = this.state;
+    this.setState({
+      isLoading: true,
+      searchValue: artist,
+    }, async () => {
+      const searchId = await searchAlbumsAPI(artist);
+      this.setState({
+        albuns: searchId,
+        isLoading: false,
+        artist: '',
+      });
+    });
+  };
+
+  render() {
+    const { artist, albuns, isLoading, searchValue } = this.state;
     const minLength = 2;
+    if (isLoading) return <Loading />;
     return (
       <div data-testid="page-search">
-        Search
-        <Header />
+
+        <header>
+          Search
+          <Header />
+        </header>
+
         <form>
+
           <label htmlFor="name">
             Artist:
             <input
@@ -34,15 +62,37 @@ class Search extends Component {
               onChange={ this.handleChange }
             />
           </label>
+
           <button
             data-testid="search-artist-button"
             type="button"
             disabled={ artist.length < minLength }
-            onClick={ this.handleClick }
+            onClick={ this.handleClickErase }
           >
             Search
           </button>
+
         </form>
+
+        {
+          (albuns.length) !== 0
+            ? (
+              <div>
+                <p>
+                  {' '}
+                  { `Resultado de álbuns de: ${searchValue}`}
+                </p>
+                { albuns.map((album) => (
+                  <Cards
+                    key={ album.collectionId }
+                    artistId={ album.artistId }
+                    searchValue={ album.collectionName }
+                    collectionId={ album.collectionId }
+                  />
+                ))}
+              </div>)
+            : <p>Nenhum álbum foi encontrado</p>
+        }
       </div>
     );
   }
